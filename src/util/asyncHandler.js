9e7c74c15c5d4fdb4ffbec75bff884f4892e1ea4
Qasim <qasimzexal@gmail.com>
2025-05-11 11:52:40 +0500

    added mutler

--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -1,14 +1,7 @@
-// const asyncHandler = () ={}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export { asyncHandler };
-
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (e) {
-    res.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-};
